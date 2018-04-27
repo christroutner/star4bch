6b96b9d89e3b372953d7e38443a1836e1e6f7dd6
Chris Troutner <chris.troutner@gmail.com>
2018-04-27 11:35:16 -0700

    minor editing

--- bin/server.js
+++ bin/server.js
@@ -7,6 +7,7 @@ const session = require('koa-generic-session')
 const passport = require('koa-passport')
 const mount = require('koa-mount')
 const serve = require('koa-static')
+const cors = require('kcors')
 
 const config = require('../config')
 const errorMiddleware = require('../src/middleware')
@@ -38,7 +39,7 @@ app.use(passport.session())
 const modules = require('../src/modules')
 modules(app)
 
-const cors = require('kcors')
+// Enable CORS for testing
 app.use(cors({origin: '*'}))
 
 // MIDDLEWARE END
